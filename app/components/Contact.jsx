4c3b7bec39dd09363f19f606f2569e2cbccfa97b
davidwanlongwu <davidwu.web@gmail.com>
2025-04-26 17:16:13 -0600

    basic contact form

---
+++ app/components/Contact.jsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+
+const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "2bf43d39-1274-4c55-947a-d439cbb51e9c");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div
+      id="contact"
+      className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
+    >
+      <h4 className="text-center mb-2 text-lg">Contact me</h4>
+      <h2 className="text-center text-5xl">Get in touch</h2>
+
+      <p className="text-center max-w-2xl mx-auto mt-t mb-12">
+        Please leave your message here. I'd love to hear from you.
+      </p>
+
+      <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
+        <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
+          <input
+            name="name"
+            type="text"
+            placeholder="Enter your name"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+        </div>
+        <textarea
+          name="message"
+          rows="4"
+          placeholder="Enter your message"
+          required
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
+        ></textarea>
+
+        <button
+          type="submit"
+          className="py-3 px-6 w-max flex items-center justify-between gap-2 bg-black/70 text-white rounded-full mx-auto hover:bg-black duration-500"
+        >
+          Send message{" "}
+          <Image
+            src={assets.right_arrow_white}
+            alt="arrow icon"
+            className="w-4"
+          />
+        </button>
+        <p className="mt-4">{result}</p>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Work = () => {
   return (
-    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20 bg-[#e4dc8a]">
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
       <h4 className="text-center mb-2 text-lg">My portfolio</h4>
       <h2 className="text-center text-5xl">My latest work</h2>
 
