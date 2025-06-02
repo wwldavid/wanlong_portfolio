42beefe526a116d4637aed8f0766f87695b07427
davidwanlongwu <davidwu.web@gmail.com>
2025-06-02 14:05:33 -0600

    change icons

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,23 +5,25 @@ import { assets } from "../assets/assets";
 const Header = ({ isDarkMode }) => {
   return (
     <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
-      <div>
-        <Image
-          src={assets.profile_img}
-          alt="profile img"
-          className=" rounded-full w-32"
-        />
+      <div className="flex gap-8 mb-6">
+        <div>
+          <Image
+            src={assets.profile_img}
+            alt="profile img"
+            className=" rounded-full w-32"
+          />
+        </div>
+        <h3 className="flex items-end gap-3 text-xl md:text-2xl">
+          Hi, I&apos;m David Wanlong Wu{" "}
+          <Image src={assets.python} alt="python icon" className="w-10" />
+        </h3>
       </div>
-      <h3 className="flex items-end gap-3 text-xl md:text-2xl mb-3">
-        Hi, I&apos;m Wanlong Wu{" "}
-        <Image src={assets.hand_icon} alt="hand icon" className="w-6" />
-      </h3>
-      <h1 className="text-3xl sm:text-6xl lg:text-[70px]">
-        frontend web developer in Alberta
+      <h1 className="text-3xl sm:text-6xl lg:text-[60px] text-[#84b9cb]">
+        Frontend web developer <br /> in Alberta.
       </h1>
       <p className="max-w-2xl mx-auto">
-        I am a frontend developer from Alberta, Canada graduated from South
-        Alberta Institute of Technology.
+        I am a frontend developer from Alberta, Canada. I am graduating from
+        South Alberta Institute of Technology on August 2025.
       </p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
         <a
