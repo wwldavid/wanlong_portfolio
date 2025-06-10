c87191bbfe2d2dd28b0c65514ae897ecb21db7af
davidwanlongwu <davidwu.web@gmail.com>
2025-06-10 08:49:04 -0600

    adjust color mode

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${playfair.variable} ${lora.variable} font-body leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+        className={`${playfair.variable} ${lora.variable} 
+        font-body leading-8 overflow-x-hidden
+        bg-[url('/images/sky4.jpg')] bg-cover bg-no-repeat bg-fixed bg-center
+         dark:bg-darkTheme dark:text-white dark:bg-none
+         `}
       >
         {children}
       </body>
